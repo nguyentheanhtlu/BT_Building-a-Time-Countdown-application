47d7ceea8ee8e0214dc2f6a4e243a5c9df689247
nguyentheanhtlu <theanh28022003@gmail.com>
2022-11-07 16:25:29 +0700

    done

---
+++ js/Main.js
@@ -0,0 +1,19 @@
+function sleep(ms){
+    return new Promise((resolve) => setTimeout(resolve,ms))
+}
+
+async function countDown(i){
+    while (i>0){
+        document.getElementById('time-countdown').innerHTML = i;
+        i--;
+        await sleep(1000);
+    }
+    return 'counter finished';
+}
+function input(){
+    let counter = countDown(+document.getElementById('number').value);
+    counter.then((msg) => {
+        document.getElementById('time-countdown').innerHTML = msg
+    });
+}
+
